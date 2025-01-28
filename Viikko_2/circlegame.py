8e4b2e6fbaff0524f66a97bd04b11a597daf7b8a
EeegorL <mikkolegezin@gmail.com>
2025-01-28 18:28:09 +0200

    fghtyuikj

--- Viikko_2/circlegame.py
+++ Viikko_2/circlegame.py
@@ -1,12 +1,28 @@
-def find_order(n):
-    pelaajat = list(range(1, n+1));
-    arr = [];
-    i = 0;
-    while pelaajat:
-        i = ((i+1) % len(pelaajat));
-        arr.append(pelaajat.pop(i));
+# I really struggled with this just for the example solution to be so easy...
+
+# def find_order(n):
+#     pelaajat = list(range(1, n+1));
+#     arr = [];
+#     i = 0;
+#     while pelaajat:
+#         i = ((i+1) % len(pelaajat)); 
+#         arr.append(pelaajat.pop(i));
     
-    return arr;
+#     return arr;
+
+def find_order(n):
+    players = list(range(1, n+1))
+    res = [];
+
+    remove = False;
+    for p in players:
+        if remove:
+            res.append(p);
+        else:
+            players.append(p);
+        remove = not remove;
+    
+    return res
 
 if __name__ == "__main__":
     print(find_order(1)) # [1]
@@ -16,18 +32,3 @@ if __name__ == "__main__":
 
     order = find_order(10**5)
     print(order[-5:]) # [52545, 85313, 36161, 3393, 68929]
-
-    # def find_order(n):
-    # players = list(range(1, n+1))
-
-    # removed = []
-    # remove_next = False
-
-    # for player in players:
-    #     if remove_next:
-    #         removed.append(player)
-    #     else:
-    #         players.append(player)
-    #     remove_next = not remove_next
-
-    # return removed
