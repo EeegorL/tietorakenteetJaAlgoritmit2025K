035af00b9de2ada9ccdda3fc6c9586b8007ddaf1
EeegorL <mikkolegezin@gmail.com>
2025-01-29 00:09:41 +0200

    kerro kerro mikitin, miten tän mä ratkaisen

--- Viikko_2/fastrounds.py
+++ Viikko_2/fastrounds.py
@@ -8,23 +8,23 @@ def count_rounds(numbers):
         isInSomeArr = False;
         for arr in arrs:
             if(x-1 in arr):
-                arr.append(x);
+                arr.add(x);
                 isInSomeArr = True;
                 break;
         if(not isInSomeArr):
-            arrs.append([x]);
-
+            s = set(); s.add(x)
+            arrs.append(s);
+    print(arrs)
     return len(arrs);
     
 
 if __name__ == "__main__":
-    # print(count_rounds([1, 2, 3, 4])) # 1
-    # print(count_rounds([1, 3, 2, 4])) # 2
-    # print(count_rounds([4, 3, 2, 1])) # 4
-    # print(count_rounds([1])) # 1
-    # print(count_rounds([2, 1, 4, 7, 5, 3, 6, 8])) # 4
+    print(count_rounds([1, 2, 3, 4])) # 1
+    print(count_rounds([1, 3, 2, 4])) # 2
+    print(count_rounds([4, 3, 2, 1])) # 4
+    print(count_rounds([1])) # 1
+    print(count_rounds([2, 1, 4, 7, 5, 3, 6, 8])) # 4
 
-    n = 10**5
-    numbers = list(reversed(range(1, n+1)))
-    print(count_rounds(numbers)) # 100000
-    ...
+    # n = 10**4
+    # numbers = list(reversed(range(1, n+1)))
+    # print(count_rounds(numbers)) # 100000
