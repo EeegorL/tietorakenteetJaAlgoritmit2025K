c7fbc735cd6638be61933c66ea56c720d3164e10
EeegorL <mikkolegezin@gmail.com>
2025-01-28 15:32:25 +0200

    skibidi

--- Viikko_2/circlegame.py
+++ Viikko_2/circlegame.py
@@ -1,21 +1,18 @@
 def find_order(n):
     pelaajat = list(range(1, n+1));
     arr = [];
-    i = 1;
-
-
-
-    while(len(pelaajat) > 0):
-        if(i > len(pelaajat)):
-            i = len(pelaajat) - 2
-        arr.append(pelaajat[i]);
-        print(i, pelaajat, pelaajat[i])
-        pelaajat.remove(i);
-        i+=1;
+    i = 0;
+    while pelaajat:
+        i = ((i+1) % len(pelaajat));
+        arr.append(pelaajat.pop(i));
+    
     return arr;
 
-
-
 if __name__ == "__main__":
-    order = find_order(4)
-    print(order)
+    print(find_order(1)) # [1]
+    print(find_order(2)) # [2, 1]
+    print(find_order(3)) # [2, 1, 3]
+    print(find_order(7)) # [2, 4, 6, 1, 5, 3, 7]
+
+    order = find_order(10**5)
+    print(order[-5:]) # [52545, 85313, 36161, 3393, 68929]
