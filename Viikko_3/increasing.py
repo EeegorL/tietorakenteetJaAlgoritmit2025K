e6f744dc62aeb75e1a93e6612f31432e57b986da
EeegorL <mikkolegezin@gmail.com>
2025-01-31 00:37:19 +0200

    yee

---
+++ Viikko_3/increasing.py
@@ -0,0 +1,27 @@
+def count_sublists(numbers):
+    risingStart = 0;
+    counting = True;
+    count = len(numbers);
+    i=1;
+    while(i < len(numbers)):
+        if(numbers[i] > numbers[i-1] and not counting):
+            risingStart = i-1;
+            counting = True;
+        else:
+            if(counting and numbers[i] <= numbers[i-1]):
+                count += i - risingStart;
+                counting = False;
+                risingStart = i;
+        i+=1;
+    return count;
+
+
+if __name__ == "__main__":
+    print(count_sublists([2, 1, 3, 4])) # 7
+    print(count_sublists([1, 2, 3, 4])) # 10
+    print(count_sublists([4, 3, 2, 1])) # 4
+    print(count_sublists([1, 1, 1, 1])) # 4
+    print(count_sublists([1, 2, 1, 2])) # 6
+
+    numbers = list(range(1, 10**5+1))
+    print(count_sublists(numbers)) # 5000050000
